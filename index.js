606cc76c3901799b4d0f7102da48eef5f6be2502
petersonwilliam97 <petersonw1997@gmail.com>
2026-07-04 18:23:42 -0300

    :tada: initial commit - CozyForecast weather app

---
+++ index.js
@@ -0,0 +1,83 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+const input = document.querySelector('.search-box input');
+
+search.addEventListener('click', () => {
+    const APIKey = '330d3742cb08ab11ba90e217732ee10b';
+    const city = input.value.trim();
+
+    if (city === '') return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            const countryCode = json.sys.country;
+            const countryNameEl = document.getElementById('country-name');
+            const countryFlagEl = document.getElementById('country-flag');
+
+            const regionNames = new Intl.DisplayNames(['pt-BR'], { type: 'region' });
+            const countryName = regionNames.of(countryCode);
+
+            countryNameEl.textContent = countryName;
+            countryFlagEl.src = `https://flagcdn.com/w40/${countryCode.toLowerCase()}.png`;
+            countryFlagEl.alt = `Bandeira de ${countryName}`;
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '660px';
+        });
+});
+
+input.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        search.click();
+    }
+});
